63116768d2187cffe8d6b322f23b582cffd1f135
BenCras <r0879936@student.thomasmore.be>
2023-03-21 09:53:41 +0100

    vraag1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './models/enums.dart' as lib;
 
 void main() {
   runApp(MainApp());
@@ -11,15 +12,56 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class Main extends StatelessWidget {
+class Main extends StatefulWidget {
   const Main({super.key});
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  String dropdownvalue = 'All';
+  var items = [
+    'All',
+    'Resistance',
+    'Sith',
+    'Jedi Order',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('In a galaxy far far away...'),
-      ),
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Star Wars Characters"),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              "Select a side:",
+              textAlign: TextAlign.left,
+            ),
+            DropdownButton(
+              value: dropdownvalue,
+              items: items.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownvalue = newValue!;
+                });
+              },
+              isExpanded: true,
+            ),
+            Text(
+              "There are 5 characters",
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ));
   }
 }
